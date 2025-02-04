2a4e0b93ac85968fac32f1a46ebcee322ea12d13
Kudzeri <kudzeri@gmail.com>
2025-02-04 14:06:15 +0500

    Добавлены категории к админке

---
+++ app/Filament/Resources/CategoryResource.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CategoryResource\Pages;
+use App\Models\Category;
+use Filament\Forms;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\TagsColumn;
+
+class CategoryResource extends Resource
+{
+    protected static ?string $model = Category::class;
+    protected static ?string $navigationIcon = 'heroicon-o-tag';
+    protected static ?string $navigationGroup = 'Товар';
+    protected static ?string $navigationLabel = 'Категории';
+
+    public static function form(Forms\Form $form): Forms\Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('slug')
+                    ->label('Slug')
+                    ->required()
+                    ->unique(Category::class, 'slug'),
+
+                TextInput::make('title')
+                    ->label('Название категории')
+                    ->required(),
+
+                Select::make('parent_id')
+                    ->label('Родительская категория')
+                    ->relationship('parent', 'title') // Связь с родительской категорией
+                    ->nullable() // Родительская категория может быть пустой
+                    ->searchable(),
+            ]);
+    }
+
+    public static function table(Tables\Table $table): Tables\Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('title')
+                    ->label('Название категории')
+                    ->searchable(),
+
+                TextColumn::make('slug')
+                    ->label('Slug')
+                    ->sortable(),
+
+                TextColumn::make('parent.title')
+                    ->label('Родительская категория')
+                    ->sortable(),
+            ])
+            ->filters([
+                // Можно добавить фильтры, например, для родительских категорий
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCategories::route('/'),
+            'create' => Pages\CreateCategory::route('/create'),
+            'edit' => Pages\EditCategory::route('/{record}/edit'),
+        ];
+    }
+}
