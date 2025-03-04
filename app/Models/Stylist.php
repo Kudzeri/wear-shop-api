eb992603a0bbe39a7faf5e2b25d6620a31ebd85d
Kudzeri <kudzeri@gmail.com>
2025-03-04 12:54:48 +0500

    добавлена схема стилиста

--- app/Models/Stylist.php
+++ app/Models/Stylist.php
@@ -4,7 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-
+/**
+ * @OA\Schema(
+ *     schema="Stylist",
+ *     title="Stylist",
+ *     description="Модель выбора стилиста",
+ *     @OA\Property(property="id", type="integer", example=1),
+ *     @OA\Property(property="image_url", type="string", example="https://example.com/stylist.jpg"),
+ *     @OA\Property(property="products", type="array", @OA\Items(ref="#/components/schemas/Product")),
+ * )
+ */
 class Stylist extends Model
 {
     protected $fillable = ['image_url'];
