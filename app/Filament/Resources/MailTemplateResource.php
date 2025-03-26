06763f8e1ca122ab46f473dd8f55fdfee00e69bc
Kudzeri <kudzeri@gmail.com>
2025-03-26 12:57:52 +0500

    реализован шаблонизатор рассылки

---
+++ app/Filament/Resources/MailTemplateResource.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\MailTemplateResource\Pages;
+use App\Filament\Resources\MailTemplateResource\RelationManagers;
+use App\Models\MailTemplate;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class MailTemplateResource extends Resource
+{
+    protected static ?string $model = MailTemplate::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->required(),
+                Forms\Components\TextInput::make('subject')
+                    ->required(),
+                Forms\Components\RichEditor::make('content')
+                    ->required()
+                    ->helperText('Используй [name] для вставки имени подписчика.'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+        ->columns([
+            Tables\Columns\TextColumn::make('title')->searchable(),
+            Tables\Columns\TextColumn::make('subject'),
+            Tables\Columns\TextColumn::make('created_at')->dateTime(),
+        ])
+        ->actions([
+            Tables\Actions\EditAction::make(),
+            Action::make('send')
+                ->label('Отправить всем')
+                ->icon('heroicon-o-paper-airplane')
+                ->color('success')
+                ->requiresConfirmation()
+                ->action(function (MailTemplate $record) {
+                    $subscribers = Subscriber::all();
+                    foreach ($subscribers as $subscriber) {
+                        Mail::to($subscriber->email)
+                            ->queue(new MassTemplateMail($record, $subscriber));
+                    }
+                })
+        ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMailTemplates::route('/'),
+            'create' => Pages\CreateMailTemplate::route('/create'),
+            'edit' => Pages\EditMailTemplate::route('/{record}/edit'),
+        ];
+    }
+}
