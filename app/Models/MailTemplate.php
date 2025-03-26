9e1d02a50b7ddf46de468ffc919c530b1aff870e
Kudzeri <kudzeri@gmail.com>
2025-03-26 12:56:25 +0500

    добавлен заголовок

--- app/Models/MailTemplate.php
+++ app/Models/MailTemplate.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class MailTemplate extends Model
 {
-    protected $fillable = ['subject', 'content'];
+    protected $fillable = ['subject', 'content', 'title'];
 }
--- database/migrations/2025_03_26_075031_create_mail_templates_table.php
+++ database/migrations/2025_03_26_075031_create_mail_templates_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('mail_templates', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
             $table->string('subject');
             $table->text('content');
             $table->timestamps();
