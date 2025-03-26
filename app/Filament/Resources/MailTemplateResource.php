1c7a840136bd10660fa02c54ef676f5217046d36
Kudzeri <kudzeri@gmail.com>
2025-03-26 13:35:07 +0500

    fix

--- app/Filament/Resources/MailTemplateResource.php
+++ app/Filament/Resources/MailTemplateResource.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\MailTemplateResource\Pages;
 use App\Filament\Resources\MailTemplateResource\RelationManagers;
 use App\Models\MailTemplate;
+use Filament\Tables\Actions\Action;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Resources\Resource;
