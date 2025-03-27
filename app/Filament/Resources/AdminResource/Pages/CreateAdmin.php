111d255e992446c5033367ffeff2cf113e889933
Kudzeri <kudzeri@gmail.com>
2025-03-27 15:28:42 +0500

    фикс логики создания администратора

--- app/Filament/Resources/AdminResource/Pages/CreateAdmin.php
+++ app/Filament/Resources/AdminResource/Pages/CreateAdmin.php
@@ -10,7 +10,7 @@ class CreateAdmin extends CreateRecord
 {
     protected static string $resource = AdminResource::class;
 
-    public function afterCreate($record): void
+    protected function afterCreate($record): void
     {
         $record->assignRole('admin');
         $record->givePermissionTo('view_filament');
