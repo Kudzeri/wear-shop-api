fbc7c9197f90848f8017479ad6f093c333fd1a7d
Kudzeri <kudzeri@gmail.com>
2025-03-27 15:31:01 +0500

    фикс логики создания администратора

--- app/Filament/Resources/AdminResource/Pages/CreateAdmin.php
+++ app/Filament/Resources/AdminResource/Pages/CreateAdmin.php
@@ -3,17 +3,22 @@
 namespace App\Filament\Resources\AdminResource\Pages;
 
 use App\Filament\Resources\AdminResource;
-use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class CreateAdmin extends CreateRecord
 {
     protected static string $resource = AdminResource::class;
 
-    protected function afterCreate($record): void
+
+    protected function handleRecordCreation(array $data): Model
     {
+        $record = static::getModel()::create($data);
+
         $record->assignRole('admin');
         $record->givePermissionTo('view_filament');
+
+        return $record;
     }
 
 }
